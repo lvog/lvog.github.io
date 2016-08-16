ed76cfc061ee50b93b35cf2370c5a7ec6fe69858
lvog <levchuk.oleg21@gmail.com>
2016-08-16 12:54:03 +0300

    Add Resto

--- js/common.js
+++ js/common.js
@@ -78,6 +78,11 @@ $('.owl-carousel').owlCarousel({
 					},
 					768:{
 						items:3,
+						nav:true,
+						loop:true
+					},
+					1200: {
+						items:4,
 						nav:false,
 						loop:false
 					}
